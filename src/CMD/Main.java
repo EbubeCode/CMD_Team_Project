8555781a817d7db230e90751034bf2ee34ee824d
Lordsolid <richardebboh@gmail.com>
2020-09-21 21:45:15 -0700

    Created Welcome Page

--- src/CMD/Main.java
+++ src/CMD/Main.java
@@ -1,23 +1,27 @@
-package sample;
+package CMD;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("welcomePage.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
---
+++ src/CMD/welcomePageController.java
@@ -0,0 +1,23 @@
+package CMD;
+
+/* Copyright (c) 2020, CMD and/or its affiliates. All rights reserved.
+ * CMD PROPRIETARY/CONFIDENTIAL. Use is subject to CMD license terms.
+*/
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class welcomePageController {
+    @FXML
+    private Label welcome_label;
+
+    @FXML
+    private Label motto_label;
+
+    @FXML
+    private Button proceed_button;
+
+    @FXML
+    private Label close_label;
+
+}
