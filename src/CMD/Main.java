b3822dc0cb7c3070037d7cf238ce7eed74c09fd2
Lordsolid <richardebboh@gmail.com>
2020-09-22 21:44:09 -0700

    Designed the Main Application stage

--- src/CMD/Main.java
+++ src/CMD/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("welcomePage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainAppPage.fxml"));
 
         Scene scene = new Scene(root);
         primaryStage.initStyle(StageStyle.UNDECORATED);
---
+++ src/CMD/MainAppPageController.java
@@ -0,0 +1,41 @@
+package CMD;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class MainAppPageController {
+    @FXML
+    private AnchorPane mainAppPane;
+
+    @FXML
+    private Label close_label;
+
+    @FXML
+    private Label minimizeLabel;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private AnchorPane menuBarPane;
+
+    @FXML
+    private Button addNewMemberButton;
+
+    @FXML
+    private Button viewMemberRecordButton;
+
+    @FXML
+    private Button addPaymentRecordButton;
+
+    @FXML
+    private Button aboutCmdButton;
+
+    @FXML
+    private AnchorPane teamMembersPane;
+
+    @FXML
+    private Label teamMembersLabel;
+}
