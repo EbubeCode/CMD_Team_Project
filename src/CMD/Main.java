bc9f5ab3563406ddf64ca708c6cf5c4d8a86769f
Lordsolid <richardebboh@gmail.com>
2020-09-23 19:10:55 -0700

    Still on resolving merge conflicts

--- src/CMD/Main.java
+++ src/CMD/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainAppPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("welcomePage.fxml"));
 
         Scene scene = new Scene(root);
         primaryStage.initStyle(StageStyle.UNDECORATED);
