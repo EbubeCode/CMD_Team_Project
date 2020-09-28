8ffe78b338840c9283e461ece82fc05328a4a134
EbubeCode <61353651+EbubeCode@users.noreply.github.com>
2020-09-28 15:21:21 +0100

    Modified the RequestHandler singleton

--- src/CMD/controller/RequestHandler.java
+++ src/CMD/controller/RequestHandler.java
@@ -20,6 +20,8 @@ public class RequestHandler {
 
     private String defaultColorLabel_X;
 
+    private Alert alert;
+
 //    Singleton Constructor
     public static RequestHandler getInstance(){
         if (mInstance == null) {
@@ -28,17 +30,29 @@ public class RequestHandler {
         return mInstance;
     }
 
-/*
-   * Method to show Alert on close
-   * This method returns a ButtonType pressed so as to work on our proceed button animation effectively.
-   * This is because we do not want our proceed button to stop animating when we press CANCEL on close.
-*/
 
-    public ButtonType handleClose(){
-        ButtonType buttonType = null;
-        Alert alert = new Alert( Alert.AlertType.NONE,"Are you sure you want to exit?",
+    /*
+     * A private constructor to ensure that an instance of this class cannot be created
+     * outside this class
+     */
+    private RequestHandler () {
+
+        /*
+         * create the alert object to handle onAction close
+         */
+        alert = new Alert( Alert.AlertType.NONE,"Are you sure you want to exit?",
                 ButtonType.YES,  ButtonType.CANCEL);
         alert.setTitle("Exit");
+    }
+
+
+    /*
+     * Method to show Alert on close
+     * This method returns a ButtonType pressed so as to work on our proceed button animation effectively.
+     * This is because we do not want our proceed button to stop animating when we press CANCEL on close.
+     */
+    public ButtonType handleClose(){
+        ButtonType buttonType = null;
         Optional<ButtonType> result = alert.showAndWait();
 
         if (result.isPresent() && (result.get() == ButtonType.YES)) {
@@ -50,11 +64,19 @@ public class RequestHandler {
         return buttonType;
     }
 
+    /*
+     * Method to change the background color of the close and minimize button when
+     * a mouse enters the Label
+     */
     public void handleMouseEntered(Label closeLabel) {
         defaultColorLabel_X = closeLabel.getStyle();
         closeLabel.setStyle("-fx-background-color: red");
     }
 
+    /*
+     * Method to change the background color of the close and minimize button when
+     * a mouse leaves the Label to default color of the Label
+     */
     public void handleMouseExited(Label closeLabel) {
         if(defaultColorLabel_X != null) {
             closeLabel.setStyle(defaultColorLabel_X);
