2da7a1dfd41abc86221c646e03be77e9220109fa
EbubeCode <61353651+EbubeCode@users.noreply.github.com>
2020-10-03 20:07:12 +0100

    Created database with a relative path

--- src/com/CMD/AddNewMemberController.java
+++ src/com/CMD/AddNewMemberController.java
@@ -1,13 +1,27 @@
 package com.CMD;
 
+import com.CMD.util.DataBaseHandler;
 import com.CMD.util.RequestHandler;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class AddNewMemberController {
     @FXML
-    public Label closeLabel;
+    private Label closeLabel;
+
+    @FXML
+    private JFXTextField fNameField, lNameField, pNumberField, emailField;
+
+    @FXML
+    private JFXDatePicker dobField;
+
 
     public void closeLabelPressed(MouseEvent mouseEvent) {
         RequestHandler.getInstance().handleCloseLabel(closeLabel);
@@ -21,5 +35,17 @@ public class AddNewMemberController {
         RequestHandler.getInstance().handleMouseExited((Label) mouseEvent.getSource());
     }
 
+    public void onRegister(ActionEvent event) {
+        String[] fields = {fNameField.getText(), lNameField.getText(), pNumberField.getText(),
+                emailField.getText(), dobField.getEditor().getText()};
+        if (!(fields[0].isEmpty() && fields[1].isEmpty() && fields[2].isEmpty() && fields[3].isEmpty() &&
+                fields[4].isEmpty())) {
+
+            Platform.runLater(() -> DataBaseHandler.getInstance().
+                    insert(fields[0], fields[1], fields[2], fields[3], fields[4], null));
+            Stage stage = (Stage) fNameField.getScene().getWindow();
+            stage.close();
+        }
+    }
 
 }
--- src/com/CMD/util/DataBaseHandler.java
+++ src/com/CMD/util/DataBaseHandler.java
@@ -2,20 +2,33 @@ package com.CMD.util;
 
 import java.sql.*;
 
+/*
+ * Singleton class for querying the database
+ */
 public class DataBaseHandler {
     private static DataBaseHandler instance;
     private String insert = "INSERT INTO members (fName, lName, phoneNumber," +
             " email, dateOfBirth, imageUrl) VALUES (?, ?, ?, ?, ?, ?)";
 
     private DataBaseHandler() {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:sqlite:src/resources/dataBase/CMD.db"
+        ); Statement statement = conn.createStatement()) {
+
+            statement.execute("CREATE TABLE IF NOT EXISTS " +
+                    "members (fName TEXT, lName Text, " +
+                    " phoneNumber Text, email TEXT, dateOfBirth Text, imageUrl TEXT)");
+        } catch (SQLException e) {
+            System.out.println("Something went wrong " + e.getMessage());
+        }
     }
 
     public void insert(String fName, String lName, String phoneNumber, String email,
                        String dateOfBirth, String imgUrl) {
 
-        try (Connection  conn = DriverManager.getConnection(
-                "jdbc:sqlite:C:\\Users\\Ebube\\IdeaProjects\\CMD_PROJECT\\src\\com\\CMD\\database\\CMD.db"
-        );  PreparedStatement statement = conn.prepareStatement(insert)){
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:sqlite:src/resources/dataBase/CMD.db"
+        ); PreparedStatement statement = conn.prepareStatement(insert)) {
 
             statement.setString(1, fName);
             statement.setString(2, lName);
@@ -25,18 +38,16 @@ public class DataBaseHandler {
             statement.setString(6, imgUrl);
             statement.execute();
 
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Something went wrong " + e.getMessage());
         }
     }
 
     public static void main(String[] args) {
-        DataBaseHandler.getInstance().insert("Chukwuma", "Ebube", "09050426383",
-                "chukwuma258@gmail.com", "08/12/1997",
-                "C:\\Users\\Ebube\\IdeaProjects\\CMD_PROJECT\\src\\resources\\image_res\\Ebube.png");
+        DataBaseHandler.getInstance();
     }
 
-     public static DataBaseHandler getInstance() {
+    public static DataBaseHandler getInstance() {
         if (instance == null)
             instance = new DataBaseHandler();
         return instance;
