8fdf7db14da887e33a2be05e0df20658431ddd0f
EbubeCode <61353651+EbubeCode@users.noreply.github.com>
2020-12-28 17:58:58 +0100

    commit before creating jar

--- src/com/CMD/encryption/EncryptionTester.java
+++ src/com/CMD/encryption/EncryptionTester.java
@@ -1,30 +1,30 @@
-package com.CMD.encryption;
-
-public class EncryptionTester {
-    public static void main(String[] args) {
-
-
-
-        try {
-            EncryptionUtil.init();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("INIT CONFIGURATIONS SUCCESSFUL\n=====================================\n");
-        String OUTPUT_FORMAT = "%-30s:%s";
-        String PASSWORD = "55G4f8,42#";
-
-        String encryptedText = EncryptionUtil.encrypt(PASSWORD);
-
-        System.out.println("\n------ AES PKC5 Password-based Encryption ------");
-        System.out.println(String.format(OUTPUT_FORMAT, "Input (plain text)", PASSWORD));
-        System.out.println(String.format(OUTPUT_FORMAT, "Encrypted (base64) ", encryptedText));
-
-        System.out.println("\n------ AES PKC5 Password-based Decryption ------");
-        System.out.println(String.format(OUTPUT_FORMAT, "Input (base64)", encryptedText));
-
-        String decryptedText = EncryptionUtil.decrypt(encryptedText);
-        System.out.println(String.format(OUTPUT_FORMAT, "Decrypted (plain text)", decryptedText));
-    }
-}
+//package com.CMD.encryption;
+//
+//public class EncryptionTester {
+//    public static void main(String[] args) {
+//
+//
+//
+//        try {
+//            EncryptionUtil.init();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        System.out.println("INIT CONFIGURATIONS SUCCESSFUL\n=====================================\n");
+//        String OUTPUT_FORMAT = "%-30s:%s";
+//        String PASSWORD = "55G4f8,42#";
+//
+//        String encryptedText = EncryptionUtil.encrypt(PASSWORD);
+//
+//        System.out.println("\n------ AES PKC5 Password-based Encryption ------");
+//        System.out.println(String.format(OUTPUT_FORMAT, "Input (plain text)", PASSWORD));
+//        System.out.println(String.format(OUTPUT_FORMAT, "Encrypted (base64) ", encryptedText));
+//
+//        System.out.println("\n------ AES PKC5 Password-based Decryption ------");
+//        System.out.println(String.format(OUTPUT_FORMAT, "Input (base64)", encryptedText));
+//
+//        String decryptedText = EncryptionUtil.decrypt(encryptedText);
+//        System.out.println(String.format(OUTPUT_FORMAT, "Decrypted (plain text)", decryptedText));
+//    }
+//}
